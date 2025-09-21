2adab1abd1ddfb9a5f09c6885af973ece65c178c
L0n4h <dolounolhan@gmail.com>
2025-09-21 22:37:04 +0200

    add BD

---
+++ gestionnaire_mdp/BDD/DB.sql
@@ -0,0 +1,20 @@
+-- SQLite 
+
+-- Table utilisateur
+-- CREATE TABLE users (
+--     id INTEGER PRIMARY KEY,
+--     username TEXT UNIQUE NOT NULL,
+--     master_password TEXT NOT NULL
+-- );
+
+-- Table des mots de passe
+-- CREATE TABLE passwords (
+--     id INTEGER PRIMARY KEY,
+--     user_id INTEGER,
+--     titre TEXT NOT NULL,
+--     username TEXT,
+--     password TEXT NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+
+
